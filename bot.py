b70e0d605dddbea10d9580ed9d7ba2217861ced2
Eugene <eugene_kotov@hotmail.com>
2016-10-17 09:15:46 +0000

    simple inline button

---
+++ bot.py
@@ -0,0 +1,15 @@
+import telebot
+
+token = "227015250:AAFoA2N9aze8NzfTq7wLq8C3uThmXxJLM5E"
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(content_types=["text"])
+def default_test(message):
+    keyboard = types.InlineKeyboardMarkup()
+    url_button = types.InlineKeyboardButton(text="Перейти на Яндекс", url="https://ya.ru")
+    keyboard.add(url_button)
+    bot.send_message(message.chat.id, "Привет! Нажми на кнопку и перейди в поисковик.", reply_markup=keyboard)
+    
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
